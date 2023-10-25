34ae1eddc4ca0bdff232fd67409568b791578878
mertonjean <47507010+mertonjean@users.noreply.github.com>
2023-10-25 01:25:30 -0500

    Create A-star.py

---
+++ A-star.py
@@ -0,0 +1,2 @@
+performs the A* search to find the optimal solution for the 8-puzzle problem using the selected heuristic. It prints various statistics such as the number of nodes visited (V), nodes generated (N), depth (d), and the branching factor (b). It also prints the path to the solution.
+
